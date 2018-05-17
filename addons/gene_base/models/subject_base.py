a19bf98a08da2334a8e777333a9d507deef2cf90
pikachu1980 <dark653855@gmail.com>
2018-05-17 13:28:34 +0800

    2018.5.17 at3208

---
+++ addons/gene_base/models/subject_base.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from odoo import api,fields,models
+
+class SubjectBase(models.Model):
+
+    _name = "subject.base"
+
+    name = fields.Char('姓名')
+    sex = fields.Selection([(1,'男'),(2,'女')])
+    age = fields.Integer('年齡')
+    case_description =  fields.Text('病例簡述')
