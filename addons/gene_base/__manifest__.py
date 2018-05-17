d6e4a488357601041cf968e0dc4de4a24ddb55fc
pikachu1980 <dark653855@gmail.com>
2018-05-17 17:21:20 +0800

    2018.5.17 at3208 增加受測者資料功能

---
+++ addons/gene_base/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Gene Base",
+    'version': '1.0',
+    'depends': [],
+    'author': "先傑電腦",
+    'website': "http://www.alltop.com/",
+    'category': '',
+    'description': """
+    初版匯入，匯出
+    """,
+    'depends': [
+        'base',
+        'report_doc'
+    ],
+    'data': [
+        'views/subject_base.xml',
+        'views/gene_base.xml',
+        'views/gene_main.xml',
+        'views/menu.xml',
+
+    ],
+    'demo': [],
+}
---
+++ addons/gene_base/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from . import gene_base
+from . import gene_main
+from . import subject_base
